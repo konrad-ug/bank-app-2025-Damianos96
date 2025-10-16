3ffc58e50c5b5be82e2cc0b3238f15333647db6c
DamianZielonka <damian.zielonka2004@interia.pl>
2025-10-16 18:16:40 +0200

    feature5

--- src/account.py
+++ src/account.py
@@ -2,8 +2,8 @@ class Account:
     def __init__(self, first_name, last_name, pesel, promo_code = None):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = 50.0 if self.is_promo_code_valid(promo_code) else 0.0
-        self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+        self.pesel = pesel if self.is_pesel_valid(pesel)  else "Invalid"
+        self.balance = 50.0 if self.pesel != "Invalid" and self.is_promo_code_valid(promo_code) and self.get_birth_year_from_pesel(pesel) > 1960 else 0.0
 
     def is_pesel_valid(self, pesel):
         if pesel and len(pesel) == 11:
@@ -14,4 +14,28 @@ class Account:
         if promo_code and promo_code.startswith("PROM_") and len(promo_code) == 8:
             return True
         return False
+    
+    def get_birth_year_from_pesel(self,pesel):
+        if len(pesel) != 11:
+            return 0
+        YY = int(pesel[0:2])
+        MM = int(pesel[2:4])
+        if 1 <= MM <= 12:
+            base = 1900
+            return base + YY
+        elif 21 <= MM <= 32:
+            base = 2000
+            return base + YY
+        elif 41 <= MM <= 52:
+            base = 2100
+            return base + YY
+        elif 61 <= MM <= 72:
+            base = 2200
+            return base + YY
+        elif 81 <= MM <= 92:
+            base = 1800
+            return base + YY
+        else:
+            return 0
+
     
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -2,16 +2,10 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", "123456789112")
+        account = Account("John", "Doe", "12345678912")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0.0
-        # if len(promo_code) == 0:
-        #     assert promo_code == None
-        # else if promo_code[0:5] != "PROM_":
-        #     assert promo_code == "Invalid"
-        # else:
-        #     assert promo_code[0:5] == "PROM_" and len(promo_code) > 8
 
     def test_pesel_too_long(self):
         account = Account("Jane", "Smith", "198421941984149")
@@ -26,7 +20,7 @@ class TestAccount:
         assert account.pesel == "Invalid"
 
     def test_valid_promo_code(self):
-        account = Account("Jane", "Smith", "12345678910", "PROM_ABC")
+        account = Account("Jane", "Smith", "04295678910", "PROM_ABC")
         assert account.balance == 50.0
     
     def test_invalid_code_prefix(self):
@@ -35,6 +29,14 @@ class TestAccount:
     
     def test_invalid_code_suffix(self):
         account = Account("Jane", "Smith", "12345678910", "PRxM_ab1")
+        assert account.balance == 0.0
+    
+    def test_valid_code_age(self):
+        account = Account("Jane", "Smith", "04295678910", "PROM_ab1")
+        assert account.balance == 50.0
+    
+    def test_invalid_code_age(self):
+        account = Account("Jane", "Smith", "12105678910", "PROM_ab1")
         assert account.balance == 0.0
         
     
