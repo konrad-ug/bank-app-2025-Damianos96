cfb252487aa3aa7ef85f86a37feead54101970b9
DamianZielonka <damian.zielonka2004@interia.pl>
2025-11-28 23:59:45 +0100

    lab07

--- tests/unit/test_account_registry.py
+++ tests/unit/test_account_registry.py
@@ -42,4 +42,12 @@ class TestAccountRegistry:
         registry.add_account(account2)
         number_of_accounts = registry.get_account_count()
         assert number_of_accounts == 2
-        
+    
+    def test_delete_account(self, registry: AccountRegistry, account):
+        registry.add_account(account)
+        result = registry.delete_account("12345678912")
+        assert result == True
+    
+    def test_delete_account_fail(self, registry: AccountRegistry, account):
+        result = registry.delete_account("11111111111")
+        assert result == False
